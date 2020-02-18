61c06a854451cad7317892e6226874715c6fe9b6
Jack Manning <jmm588@miami.edu>
2020-02-17 21:54:38 -0500

    Hello World

---
+++ Food Savior/Food Savior/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Food Savior
+//
+//  Created by Jack Manning on 2/17/20.
+//  Copyright © 2020 Jack Manning. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
