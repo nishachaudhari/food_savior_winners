37e2a78c1f78e441065a5fb99ce28e3cdca5fbd0
Nisha <nishachaudhari98@gmail.com>
2020-04-02 21:47:50 -0400

    help/support topics
    put in framework for support

--- my_app/lib/screens/help/help.dart
+++ my_app/lib/screens/help/help.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/screens/help/accountHelp.dart';
+import 'package:my_app/screens/help/corona.dart';
+import 'package:my_app/screens/help/guide.dart';
+import 'package:my_app/screens/help/orders.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:my_app/screens/help/corona.dart';
 
 
 class help extends StatefulWidget
@@ -12,8 +17,17 @@ class _help extends State<help>
 {
 
 
+  void sendText(String text) async {
+    var url = 'sms:$text';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   void sendEmail(String email) async {
-    var url = 'sms:9542492027';
+    var url = 'mailto:$email';
     if (await canLaunch(url)) {
       await launch(url);
     } else {
@@ -41,18 +55,107 @@ class _help extends State<help>
                   child: Column(
                       children: <Widget>[
                       SizedBox(height: 20),
-                      FlatButton(
-                        child: Text("FAQs"),
-                        onPressed: (){},
-                      ),
-                      FlatButton(
-                        child: Text("Contact"),
+                      Text("Popular Answers", style: (TextStyle (fontSize: 20)),),
+                      Container(
+                        height:1.0,
+                        width:400.0,
+                        color:Colors.black,),
+                      SizedBox(height: 20),
+                      RaisedButton(
                         onPressed: (){
-                          sendEmail('nxc439@miami.edu');
-                        }
-                      )
+                          Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => corona()));
+                        },
+                        child: Row (children: <Widget>[
+                          Text("CoronaVirus (COVID-19) Response", style: (TextStyle(fontSize: 20))),
+                          Icon(Icons.keyboard_arrow_right),
+                          ],
+                          )
+                      ),
+                      SizedBox(height: 20),
+                      Text("Browse Help Topics", style: (TextStyle (fontSize: 20)),),
+                      Container(
+                        height:1.0,
+                        width:400.0,
+                        color:Colors.black,),
+                      SizedBox(height: 20),
+                      RaisedButton(
+                      onPressed: (){
+                        Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => guide()));
 
-                      ]
+                      },
+                      child: Row (children: <Widget>[
+                        Text("Guide to FoodSavior", style: (TextStyle(fontSize: 20))),
+                        Icon(Icons.keyboard_arrow_right),
+                        ],
+                        )
+                      ),
+                      SizedBox(height: 20),
+                      RaisedButton(
+                      onPressed: (){
+                        Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => accountHelp()));
+                      },
+                      child: Row (children: <Widget>[
+                        Text("Account", style: (TextStyle(fontSize: 20))),
+                        Icon(Icons.keyboard_arrow_right),
+                        ],
+                        )
+                      ),
+                      SizedBox(height: 20),
+                      RaisedButton(
+                      onPressed: (){
+                        Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => orderHelp()));
+                      },
+                      child: Row (children: <Widget>[
+                        Text("Orders", style: (TextStyle(fontSize: 20))),
+                        Icon(Icons.keyboard_arrow_right),
+                        ],
+                        )
+                      ),
+                      SizedBox(height: 20),
+                      RaisedButton(
+                      onPressed: (){},
+                      child: Row (children: <Widget>[
+                        Text("Trust and Safety", style: (TextStyle(fontSize: 20))),
+                        Icon(Icons.keyboard_arrow_right),
+                        ],
+                        )
+                      ),
+                      SizedBox(height: 20),
+                      Text("Contact us", style: (TextStyle (fontSize: 20)),),
+                      Container(
+                        height:1.0,
+                        width:400.0,
+                        color:Colors.black,),
+                      SizedBox(height: 20),
+                      RaisedButton(
+                        onPressed: (){sendText('9542492027');},
+                        child: Row (children: <Widget>[
+                          Icon(Icons.phone_iphone),
+                          Text("SMS", style: (TextStyle(fontSize: 20))),
+                          Icon(Icons.keyboard_arrow_right),
+                          ],
+                          )
+                      ),
+                      SizedBox(height: 20),
+                      RaisedButton(
+                        onPressed: (){sendEmail('nxc439@miami.edu');},
+                        child: Row (children: <Widget>[
+                          Icon(Icons.email),
+                          Text("Email", style: (TextStyle(fontSize: 20))),
+                          Icon(Icons.keyboard_arrow_right),
+                          ],
+                          )
+                      ),
+                      
+                    ]
                  )
                 )
                ]
