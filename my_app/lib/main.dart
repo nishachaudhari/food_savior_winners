490f7409306c2d890232c0cf66dfd9013d5b0921
Nisha <nishachaudhari98@gmail.com>
2020-06-26 23:11:46 -0400

    idek

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -30,8 +30,8 @@ class MyApp extends StatelessWidget {
           primaryColor: Color(0xFF94AE3F),         // light forest green
           backgroundColor: Color(0xFF243754),      // dark calm blue
           accentColor: Color(0xFFEBEBEB),    // very light grey white
-          canvasColor: Color(0xFF94AE3F),
-
+          //canvasColor: Color(0xFF94AE3F),
+          canvasColor: Color(0xFFEBEBEB),
           // Define the default font family.
           fontFamily: 'Roboto',
         ),
